3766cad817955b8fc1f9a5c85683ad366ca2d8d4
rabia <rabiaabdulrauf18@gmail.com>
2025-02-11 18:00:30 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+async function getData() {
+    const url = "https://api.nationalize.io/?name=nathaniel";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+  
