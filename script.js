9f45e7e4f8ce95c2773ebcf0a66892d816115be9
rabia <rabiaabdulrauf18@gmail.com>
2025-02-12 11:58:28 +0500

    updated

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 async function getData() {
-    const url = "https://api.nationalize.io/?name=nathaniel";
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-  
-      const json = await response.json();
-      console.log(json);
-    } catch (error) {
-      console.error(error.message);
-    }
-  };
+    const response = await fetch ("https://api.nationalize.io/?name=nathaniel");
+    var data= await response.json();
+
+    console.log(data);
+    document.querySelector("#Company").innerHTML = data.country[0].country_id;
+    document.querySelector("#Company").innerHTML = data.country[0].country_id;
+
+    // document.getElementById("#Country").innerHTML = data.result[0].CountryName;
+    document.getElementById("#Contact").innerHTML = data.name[0].ContactName;
+}
+getData();
+
+ 
+
   
